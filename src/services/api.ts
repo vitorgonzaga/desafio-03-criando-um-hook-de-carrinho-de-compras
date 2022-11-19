57483d17c19378934be2554ef480455571e40483
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-11-18 21:30:20 -0300

    feat(api): add some functions (not used)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,26 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
+
+
+export const getProductById = async (id: number) => {
+  try {
+    const { data } = await api.get(`products/${id}`)
+    return data
+  } catch (e) {
+    console.log('getProductById -> e', e)
+    toast.error('Erro na adição do produto')
+  }
+}
+export const getStockById = async (id: number) => {
+  try {
+    const { data } = await api.get(`stock/${id}`)
+    return data
+  } catch (e) {
+    console.log('getStockById -> e', e)
+    toast.error('Erro na adição do produto')
+  }
+}
